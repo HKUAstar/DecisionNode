ed61315a4dd40017b4a134dab6e57c9107edee40
ZOT-217 <1943197939@qq.com>
2026-05-26 17:49:44 +0800

    Stop tracking build artifacts (Groot/, build/, devel/)

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/decision_ws/src
-# Build directory: /mnt/d/decision_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("decision_node")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/mnt/d/decision_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/mnt/d/decision_ws/devel;/opt/ros/noetic'.split(';') if '/mnt/d/decision_ws/devel;/opt/ros/noetic' != '' else []
